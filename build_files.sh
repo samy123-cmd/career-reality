0db38847f1834ab2b249619b5e1529117fe3053f
Parth Mishra <samfischer14aug@gmail.com>
2026-01-02 21:12:56 +0530

    fix(build): remove migration steps from build script to ensure collectstatic runs

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
+# Build script
 echo "Building the project..."
 pip install -r requirements.txt
-echo "Make Migration..."
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
+
 echo "Collect Static..."
 python manage.py collectstatic --noinput --clear
+
 echo "Build End"
